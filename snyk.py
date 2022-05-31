d39ddd73dc5a6dca407b83ed93e29616e2d15b5c
Randy Johnston <randy.johnston@snyk.io>
2022-05-31 08:38:10 -0500

    run npm install

--- snyk.py
+++ snyk.py
@@ -57,8 +57,8 @@ def snyk_test():
     os.chdir(REPOSITORY)
     if 'package-lock.json' in PATH or 'yarn.lock' in PATH:
         print('Vulnerability scanning using lockfile ({})'.format(PATH))
-    elif 'package.json' in PATH:
-        print('No lockfile specified, running npm install')
+    if 'package.json' in PATH:
+        print('Running npm install')
         subprocess.run(['npm', 'install', '-s'])
     command = ['snyk', 'test', '--json', '--org={}'.format(ORG), '--project-name={}'.format(REPOSITORY_SLUG)]
     if PATH:
